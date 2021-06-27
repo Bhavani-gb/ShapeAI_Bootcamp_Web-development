0b84ae0eec68ebf9211beb403e32272ccd5cfe76
Bhavani-gb <86285591+Bhavani-gb@users.noreply.github.com>
2021-06-27 18:01:54 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p style={{ backgroundColor: "black" }}>
+        copyright by Gudimanikyam Ganga Bhavani @ {new Date().getFullYear()}
+      </p>
+      ;
+    </footer>
+  );
+}
+export default Footer;
---
+++ info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function info() {
+  return (
+    <div className="note">
+      <h1 style={{ backgroundColor: "yellowgreen" }}>
+        {" "}
+        JAVASCRIPT AND REACT.JS
+      </h1>
+      <p style={{ backgroundColor: "lightpink" }}>
+        Basic web development with react js
+      </p>
+    </div>
+  );
+}
+export default info;
